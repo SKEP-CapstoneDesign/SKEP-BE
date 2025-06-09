beab8f523217b0fbd5531757b750445ca0f30d57
dohyeon1231 <dohyeon1231@naver.com>
2025-06-09 16:37:22 +0900

    feat: 마이페이지 기능 구현
    닉네임 수정, 비밀번호 변경 api 구현

--- src/main/java/com/example/myapi/security/JwtAuthenticationFilter.java
+++ src/main/java/com/example/myapi/security/JwtAuthenticationFilter.java
@@ -1,5 +1,7 @@
 package com.example.myapi.security;
 
+import com.example.myapi.dto.ErrorResponse;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
@@ -23,21 +25,32 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
     protected void doFilterInternal(HttpServletRequest request,
                                     HttpServletResponse response,
                                     FilterChain filterChain) throws ServletException, IOException {
+        try {
+            String token = jwtUtil.resolveToken(request);
 
-        String token = jwtUtil.resolveToken(request);
+            if (token != null && jwtUtil.isTokenValid(token)) {
+                Long userId = jwtUtil.getUserIdFromToken(token);
 
-        if (token != null && jwtUtil.isTokenValid(token)) {
-            Long userId = jwtUtil.getUserIdFromToken(token);
+                UserDetailsImpl userDetails = new UserDetailsImpl(jwtUtil.getUserFromUserId(userId));
+                UsernamePasswordAuthenticationToken authentication =
+                        new UsernamePasswordAuthenticationToken(userDetails, null, userDetails.getAuthorities());
 
-            UserDetailsImpl userDetails = new UserDetailsImpl(jwtUtil.getUserFromUserId(userId));
-            UsernamePasswordAuthenticationToken authentication =
-                    new UsernamePasswordAuthenticationToken(userDetails, null, userDetails.getAuthorities());
+                authentication.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
+                SecurityContextHolder.getContext().setAuthentication(authentication);
+            }
 
-            authentication.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
-            SecurityContextHolder.getContext().setAuthentication(authentication);
+            filterChain.doFilter(request, response);
+
+        } catch (Exception e) {
+            // 잘못된 토큰 등 예외 발생 시 JSON 응답 전송
+            response.setStatus(HttpServletResponse.SC_FORBIDDEN); // 또는 SC_UNAUTHORIZED
+            response.setContentType("application/json;charset=UTF-8");
+
+            ErrorResponse errorResponse = new ErrorResponse("Forbidden", "유효하지 않은 토큰입니다.");
+            ObjectMapper objectMapper = new ObjectMapper();
+            String json = objectMapper.writeValueAsString(errorResponse);
+
+            response.getWriter().write(json);
         }
-
-
-        filterChain.doFilter(request, response);
     }
 }
